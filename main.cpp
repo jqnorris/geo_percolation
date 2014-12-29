5d4046961dc40d1256256708054437f6e97ede8b
J. Quinn Norris <jqnorris@ucdavis.edu>
2014-12-29 10:58:13 -0800

    3D cubic lattice added.

--- main.cpp
+++ main.cpp
@@ -436,6 +436,270 @@ public:
     }
 };
 
+// 3D infinite cubic lattice
+class unbound_cubic_Lattice_3D: public Lattice
+{
+private:
+    class location
+    {
+    public:
+        int loc[3];
+
+        location() {};
+        location(int first, int second, int third)
+        {
+            loc[0] = first;
+            loc[1] = second;
+            loc[2] = third;
+        }
+        bool operator< (const location & other) const
+        {
+            if(loc[0] < other.loc[0]) return true;
+            if(other.loc[0] < loc[0]) return false;
+            if(loc[1] < other.loc[1]) return true;
+            if(other.loc[1] < loc[1]) return false;
+            if(loc[2] < other.loc[2]) return true;
+            return false;
+        }
+    };
+
+    location current_site;
+    int next_neighbor;
+    std::map<location, Site *> sites_by_loc;
+    std::map<Site *, location> sites_by_ptr;
+
+    class Parent
+    {
+    public:
+        Site * site;
+        long int chemical_level;
+        void update(Site* this_site, long int this_chemical_level)
+        {
+            site = this_site;
+            chemical_level = this_chemical_level;
+
+        }
+        Parent()
+        {
+            site = NULL;
+            chemical_level = -1;
+        }
+
+        Parent(Site* this_site, long int this_chemical_level)
+        {
+            update(this_site, this_chemical_level);
+        }
+    };
+
+    std::map<Site *, Parent> network;
+
+    Site * get_site_ptr(location loc, int * new_site = NULL)
+    {
+        Site * loc_ptr;
+
+        if(sites_by_loc.count(loc) > 0)
+        {
+            loc_ptr = sites_by_loc[loc];
+
+            if(new_site != NULL)
+            {
+                *new_site = 0;
+            }
+        }
+        else
+        {
+            loc_ptr = sim->Site_ptr->make_site(sim);
+            sites_by_loc.insert(std::make_pair(loc, loc_ptr));
+            sites_by_ptr.insert(std::make_pair(loc_ptr, loc));
+
+            if(new_site != NULL)
+            {
+                *new_site = 1;
+            }
+        }
+
+        return loc_ptr;
+    }
+
+public:
+    ~unbound_cubic_Lattice_3D()
+    {
+        std::map<Site *, location>::iterator to_delete;
+
+        for(to_delete = sites_by_ptr.begin(); to_delete != sites_by_ptr.end(); to_delete++)
+        {
+            delete to_delete->first;
+        }
+    }
+
+    Site * get_origin(void)
+    {
+        location origin(0, 0, 0);
+
+        return get_site_ptr(origin);
+    }
+
+    void set_current_site(Site * site)
+    {
+        current_site = sites_by_ptr[site];
+        next_neighbor = 0;
+    }
+
+    bool more_neighbors()
+    {
+        if(next_neighbor < 6) return true;
+
+        return false;
+    }
+
+    Site * get_next_neighbor(int * new_site = NULL)
+    {
+        location neighbor = current_site;
+
+        switch(next_neighbor)
+        {
+        case 0: // Up
+            next_neighbor++;
+            neighbor.loc[1] += 1;
+            return get_site_ptr(neighbor, new_site);
+        case 1: // Down
+            next_neighbor++;
+            neighbor.loc[1] -= 1;
+            return get_site_ptr(neighbor, new_site);
+        case 2: // Left
+            next_neighbor++;
+            neighbor.loc[0] -= 1;
+            return get_site_ptr(neighbor, new_site);
+        case 3: // Right
+            next_neighbor++;
+            neighbor.loc[0] += 1;
+            return get_site_ptr(neighbor, new_site);
+        case 4: // Front
+            next_neighbor++;
+            neighbor.loc[2] += 1;
+            return get_site_ptr(neighbor, new_site);
+        case 5: // Front
+            next_neighbor++;
+            neighbor.loc[2] -= 1;
+            return get_site_ptr(neighbor, new_site);
+        }
+    }
+
+    double get_euclidian_distance(Site * site_1, Site * site_2 = NULL)
+    {
+        double distance;
+        location loc_1 = sites_by_ptr[site_1];
+
+
+        if (site_2 == NULL)
+        {
+
+            distance = sqrt(loc_1.loc[0]*loc_1.loc[0] + loc_1.loc[1]*loc_1.loc[1] + loc_1.loc[2]*loc_1.loc[2]);
+        }
+        else
+        {
+            location loc_2 = sites_by_ptr[site_2];
+
+            double diff_x = loc_1.loc[0] - loc_2.loc[0];
+            double diff_y = loc_1.loc[1] - loc_2.loc[1];
+            double diff_z = loc_1.loc[2] - loc_2.loc[2];
+
+            distance = sqrt(diff_x*diff_x + diff_y*diff_y + diff_z*diff_z);
+        }
+
+        return distance;
+    }
+
+    void initialize_network(void)
+    {
+        Parent temp;
+
+        for(Bond * current_bond = sim->Algorithm_ptr->get_network_begin();
+            sim->Algorithm_ptr->more_network();
+            current_bond = sim->Algorithm_ptr->get_network_next())
+        {
+            if (current_bond->first == get_origin())
+            {
+                temp.update(current_bond->first, 0);
+            }
+            else
+            {
+                temp.update(current_bond->first, (network[current_bond->first]).chemical_level+1);
+            }
+            network.insert(std::make_pair(current_bond->second, temp));
+        }
+
+    }
+
+    long int get_chemical_distance(Site * site_1, Site * site_2 = NULL)
+    {
+        if(site_2 == NULL or site_2 == get_origin())
+        {
+            return network[site_1].chemical_level;
+        }
+        else if (site_1 == get_origin())
+        {
+            return network[site_2].chemical_level;
+        }
+        else
+        {
+            Parent ancestor_1 = network[site_1];
+            Parent ancestor_2 = network[site_2];
+
+
+            long int chem_level_1 = ancestor_1.chemical_level+1;
+            long int chem_level_2 =  ancestor_2.chemical_level+1;
+
+
+            while(ancestor_1.chemical_level > ancestor_2.chemical_level)
+            {
+                ancestor_1 = network[ancestor_1.site];
+
+            }
+            while(ancestor_1.chemical_level < ancestor_2.chemical_level)
+            {
+                ancestor_2 = network[ancestor_2.site];
+
+            }
+
+            while(ancestor_1.site != ancestor_2.site)
+            {
+                ancestor_1 = network[ancestor_1.site];
+                ancestor_2 = network[ancestor_2.site];
+            }
+
+            return (chem_level_1 - ancestor_1.chemical_level) + (chem_level_2 - ancestor_2.chemical_level);
+        }
+    }
+
+    Site * get_upstream(Site * site)
+    {
+        return network[site].site;
+    }
+
+    void write_bond(std::ofstream & file, Bond * & bond)
+    {
+        location site = sites_by_ptr[bond->first];
+
+        file << bond->get_strength() << "\t" << site.loc[0] << "\t" << site.loc[1] << "\t" << site.loc[2] << "\t";
+
+        site = sites_by_ptr[bond->second];
+
+        file << site.loc[0] << "\t" << site.loc[1] << "\t" << site.loc[2] << "\n";
+
+    }
+
+
+    bool on_any_fault(Bond * bond)
+    {
+        return false;
+    }
+    double get_fault_fraction(Bond * bond)
+    {
+        return 1;
+    }
+};
+
 
 // 2D infinite square lattice with simple faults
 class unbound_square_Lattice_2D_with_faults: public Lattice
@@ -1468,10 +1732,13 @@ int main(int argc, char **argv)
     algorithm.sim = & current_sim;
     current_sim.Algorithm_ptr = & algorithm;
 
-    // unbound_square_Lattice_2D_with_faults lattice;
-    unbound_square_Lattice_2D lattice;
-    lattice.sim = & current_sim;
+    // unbound_square_Lattice_2D_with_faults lattice;    
     // lattice.add_fault(atof(argv[2]), atoi(argv[3]), 'v');
+
+    // unbound_square_Lattice_2D lattice;
+
+    unbound_cubic_Lattice_3D lattice;
+    lattice.sim = & current_sim;
 
     current_sim.Lattice_ptr = & lattice;
 
@@ -1500,17 +1767,17 @@ int main(int argc, char **argv)
 
     stats = new Statistics(&current_sim);
 
-    //stats->get_burst_distribution();
+    stats->get_burst_distribution();
 
-    //stats->write_burst_distribution_to_file();
+    stats->write_burst_distribution_to_file();
 
-    //stats->get_mass_r_distribution();
+    stats->get_mass_r_distribution();
 
-    //stats->write_mass_r_distribution_to_file();
+    stats->write_mass_r_distribution_to_file();
 
-    //stats->get_mass_l_distribution();
+    stats->get_mass_l_distribution();
 
-    //stats->write_mass_l_distribution_to_file();
+    stats->write_mass_l_distribution_to_file();
 
     stats->get_branching_statistics();
 
